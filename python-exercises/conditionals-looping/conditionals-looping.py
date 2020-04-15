fb86a07753b137d290da53b782dc974cde1836e8
Lee Assam <leeassam@gmail.com>
2020-04-15 01:59:16 -0400

    Added documentation to second challenge on conditionals and looping

--- python-exercises/conditionals-looping/conditionals-looping.py
+++ python-exercises/conditionals-looping/conditionals-looping.py
@@ -1,21 +1,34 @@
-
+'''
+Using the range(1,101), make two lists, one containing all even numbers and the other containing all odd numbers
+'''
 odd = []
 even = []
-for n in range(1, 101):
+for n in range(1,101):
     if n % 2 != 0 :
         odd.append(n)
     else :
         even.append(n)
 
 print("Odd Numbers: ", odd)
-print("Even Numbers: ", even)
+print ("Even Numbers: ", even)
 
+'''
+Given an arbitrary list containing ints, strings and floats, make three lists to store them separately
 
-myList = [1, 2, "one", "two", "three", 1.5, 3.5]
-result = { "Ints" : [], "Strings" : [], "Floats" : []}
+Input:
+myList = [1,2,”one”,”two”, “three”, 1.5, 3.5]
 
-for n in myList :
-    if isinstance(n, int) :
+Output:
+Ints [1,2]
+Strings [“one”,”two”, “three”]
+Floats [1.5, 3.5]
+'''
+
+myList = [1,2,"one","two", "three", 1.5, 3.5]
+result = {"Ints" : [], "Strings" : [], "Floats": []}
+
+for n in myList:
+    if isinstance(n, int):
         result["Ints"].append(n)
     elif isinstance(n, float) :
         result["Floats"].append(n)
