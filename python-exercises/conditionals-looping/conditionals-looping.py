7b700556abe527e9832b5263533ebb29a85fe4a3
Lee Assam <leeassam@gmail.com>
2020-04-15 01:57:27 -0400

    Added solution to second challenge on conditionals and looping

--- python-exercises/conditionals-looping/conditionals-looping.py
+++ python-exercises/conditionals-looping/conditionals-looping.py
@@ -9,3 +9,17 @@ for n in range(1, 101):
 
 print("Odd Numbers: ", odd)
 print("Even Numbers: ", even)
+
+
+myList = [1, 2, "one", "two", "three", 1.5, 3.5]
+result = { "Ints" : [], "Strings" : [], "Floats" : []}
+
+for n in myList :
+    if isinstance(n, int) :
+        result["Ints"].append(n)
+    elif isinstance(n, float) :
+        result["Floats"].append(n)
+    elif isinstance(n, str) :
+        result["Strings"].append(n)
+
+print(result)
